a591db4fae8af28d54cb37b669f44f6332c9383b
Malik Ismail <malikismail.0402@gmail.com>
2024-09-12 19:50:39 +1000

    Change fonts

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Calendar from "react-calendar";
 import moment from "moment";
@@ -6,6 +6,12 @@ import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 import malikdp from "../../assets/images/malikdp.jpeg";
 
 const CalendarPage = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (date) => {
+    setDate(date);
+  };
+
   const mom = new moment();
   mom.set("month", 8);
 
@@ -15,17 +21,17 @@ const CalendarPage = () => {
   return (
     <div className="overflow-hidden w-full h-screen relative">
       <div className="bg-brown w-[700px] -top-[215px] -left-[100px] h-[1400px] rounded-r-full absolute"></div>
-      <div className="p-[50px] w-full h-screen">
-        <div className="w-full h-[700px] relative mb-[50px] flex flex-row">
+      <div className="px-[50px] pt-[100px] w-full h-screen">
+        <div className="w-full h-[650px] relative mb-[50px] flex flex-row">
           <div className="w-[300px] bg-base mr-[50px] my-auto">
-            <Calendar />
+            <Calendar onChange={onChange} value={date} />
           </div>
-          <div className="h-full flex flex-col border-4 border-darkbrown rounded-xl grow bg-base bg-opacity-70 overflow-hidden">
+          <div className="h-full flex flex-col border-4 border-darkbrown rounded-3xl grow bg-base bg-opacity-70 overflow-hidden">
             <div className="w-full h-[125px] border-b border-b-brown flex flex-row items-center justify-between px-8">
-              <p className="text-2xl font-bold ms-10 flex">
-                <CaretLeft size={32} className="me-3" /> September 2024{" "}
+              <h2 className="text-3xl font-bold ms-10 flex">
+                <CaretLeft size={32} className="me-3" /> SEPTEMBER 2024{" "}
                 <CaretRight size={32} className="ms-3" />
-              </p>
+              </h2>
               <div className="flex flex-row gap-16">
                 <div className="flex">
                   <div className="bg-gray w-[300px] h-[50px] rounded-full flex flex-row">
@@ -40,9 +46,9 @@ const CalendarPage = () => {
                     </div>
                   </div>
                 </div>
-                <Link to="/">
+                <Link to="/" className="relative -top-1">
                   <div className="bg-pink rounded-full w-[130px] h-[50px] flex justify-center shadow-3xl absolute z-10">
-                    <p className="text-2xl my-auto">ADD</p>
+                    <h3 className="text-2xl my-auto">ADD</h3>
                   </div>
                   <div className="bg-black rounded-full w-[130px] h-[50px] flex justify-center shadow-3xl relative z-0 top-1 left-1"></div>
                 </Link>
@@ -60,9 +66,9 @@ const CalendarPage = () => {
           </div>
         </div>
         <div className="w-full h-[70px] relative flex items-center justify-between">
-          <Link to="/">
+          <Link to="/" className="relative -top-1">
             <div className="bg-pink rounded-full w-[170px] h-[50px] flex justify-center shadow-3xl absolute z-10">
-              <p className="text-2xl my-auto">BACK</p>
+              <h3 className="text-2xl my-auto">BACK</h3>
             </div>
             <div className="bg-black rounded-full w-[170px] h-[50px] flex justify-center shadow-3xl relative z-0 top-1 left-1"></div>
           </Link>
