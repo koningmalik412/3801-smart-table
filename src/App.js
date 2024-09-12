f472d91c283989199a88f37cbfb42ccc72641cef
ytamirokate <ytamirokate@gmail.com>
2024-09-12 17:43:37 +1000

    Updated Homepage, Header & Footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ import Calendar from "./pages/Calendar";
 import Dashboard from "./pages/Dashboard";
 import Games from "./pages/Games";
 import Profiles from "./pages/Profiles";
+import Settings from "./pages/Settings";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
 
 function App() {
+  const navLinks = [
+    { path: '/', text: 'Home' },
+    { path: '/dashboard', text: 'Dashboard' },
+    { path: '/calendar', text: 'Calendar' },
+    { path: '/games', text: 'Games' },
+    { path: '/profiles', text: 'Profiles' },
+    { path: '/settings', text: 'Settings' },
+  ];
+
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/calendar" element={<Calendar />} />
-      <Route path="/profiles" element={<Profiles />} />
-      <Route path="/games" element={<Games />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+      
+      <Header navLinks={navLinks} />
+      <Footer />
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      base: "#FBF6E3",
-      brown: "#60534F",
-      pink: "#EFB4B4",
-      yellow: "#FDF5B8",
-      gray: "#C6C6C0",
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        pathway: ['Pathway Gothic One', 'sans-serif'],
+      },
+      colors: {
+        base: "#FBF6E3",
+        brown: "#60534F",
+        pink: "#EFB4B4",
+        yellow: "#FDF5B8",
+        gray: "#C6C6C0",
+        blue: "#9092A8",
+      },
     },
   },
   plugins: [],
