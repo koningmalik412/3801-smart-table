d32d77e1ade28562e8cb66a1e2f047c66f241f81
Maia Jøsang <josangmaia@gmail.com>
2024-09-17 15:58:19 +1000

    Started settings page

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Settings = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <div className="pt-[50px] pl-[50px] pr-[50px]"> 
+      <div className="block w-full p-6 bg-brown rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)]">  
+        <h5 className="text-8xl font-bold text-[#FBF6E3]">SETTINGS</h5>
+      </div>
+      <div className="flex mt-4">
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
