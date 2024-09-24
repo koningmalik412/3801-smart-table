b8aebb701db9b1f419b049874f278a86a9e11649
ytamirokate <ytamirokate@gmail.com>
2024-09-24 16:09:04 +1000

    Added fonts

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -12,63 +12,63 @@ const Homepage = () => {
           <div className="flex h-[500px] gap-4">
             <Link to="/calendar" className="w-4/6">
               <div className="h-full p-10 w-full rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-base">
-                <h3 className="text-6xl text-brown font-bold ml-7 font-pathway">
+                <h3 className="text-9xl text-brown font-bold ml-7 font-pathway">
                   Calendar
                 </h3>
-                <h4 className="text-4xl text-[#7B7E9D] font-bold mb-2 ml-7 italic font-pathway">
+                {/* <h4 className="text-4xl text-[#7B7E9D] font-bold mb-2 ml-7 italic font-pathway">
                   Scheduling and Planning
-                </h4>
-                <p className="text-xl mt-5 mb-2 pt-4 pr-6 ml-7 text-brown font-montserrat">
+                </h4> */}
+                {/* <p className="text-xl mt-5 mb-2 pt-4 pr-6 ml-7 text-brown font-montserrat">
                   Create and manage schedules, and view the latest events on a
                   timeline. Your family can come together, sit down, and discuss
                   schedules collaboratively.
-                </p>
+                </p> */}
               </div>
             </Link>
             <Link to="/profiles" className="w-2/6">
               <div className="h-full p-9 w-full rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-base">
-                <h3 className="text-3xl text-brown font-bold ml-3 font-pathway">
+                <h3 className="text-9xl text-brown font-bold ml-3 font-pathway">
                   Profiles
                 </h3>
-                <h4 className="text-lg text-[#7B7E9D] font-bold mb-2 ml-3 italic font-pathway">
+                <h4 className="text-4xl text-[#7B7E9D] font-bold mb-2 ml-3 italic font-pathway">
                   Family profiles
                 </h4>
-                <p className="text-sm mt-7 mb-2 ml-3 mr-2 text-brown font-montserrat">
+                {/* <p className="text-sm mt-7 mb-2 ml-3 mr-2 text-brown font-montserrat">
                   Manage family members and their details, including adding,
                   editing, and deleting.
-                </p>
+                </p> */}
               </div>
             </Link>
           </div>
           <Link to="/games">
             <div className="h-[620px] w-full mt-6 p-10 rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-pink">
-              <h3 className="text-3xl font-bold text-brown ml-7 font-pathway">
+              <h3 className="text-9xl font-bold text-brown ml-7 font-pathway">
                 Games
               </h3>
-              <h4 className="text-lg text-[#7B7E9D] font-bold mb-2 italic ml-7 font-pathway">
+              <h4 className="text-4xl text-[#7B7E9D] font-bold mb-2 italic ml-7 font-pathway">
                 Board games
               </h4>
-              <p className="text-sm mt-5 mb-2 ml-7 text-brown font-montserrat">
+              {/* <p className="text-sm mt-5 mb-2 ml-7 text-brown font-montserrat">
                 Use the table as a space to play board games together.
-              </p>
+              </p> */}
             </div>
           </Link>
         </div>
         <div className="w-2/6 p-4">
           <Link to="/community">
             <div className="h-[1000px] w-full p-10 rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-yellow">
-              <h3 className="text-3xl font-bold text-brown ml-7 font-pathway">
+              <h3 className="text-9xl font-bold text-brown ml-7 font-pathway">
                 Community Board
               </h3>
-              <h4 className="text-lg text-[#7B7E9D] font-bold mb-2 italic ml-7 font-pathway">
+              <h4 className="text-4xl text-[#7B7E9D] font-bold mb-2 italic ml-7 font-pathway">
                 Event dashboard
               </h4>
-              <p className="text-sm mb-2 mt-10 pt-4 ml-7 mr-7 text-brown font-montserrat">
+              {/* <p className="text-sm mb-2 mt-10 pt-4 ml-7 mr-7 text-brown font-montserrat">
                 All important events and dates are posted in the form of
                 post-it-notes, allowing for a quick view of family schedules.
                 The events and dates created can also be added and displayed on
                 the standby screen.
-              </p>
+              </p> */}
             </div>
           </Link>
         </div>
