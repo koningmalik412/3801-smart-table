190152cc1e163e31164fa1b25e2e3234d87c4de6
Maia Jøsang <josangmaia@gmail.com>
2024-09-24 14:38:45 +1000

    Started changing homepage dimensions

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -5,20 +5,20 @@ const Homepage = () => {
   return (
     <div className="pt-[50px] pl-[50px] pr-[50px] relative">
       <div className="block w-full p-6 bg-brown rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)]">
-        <h5 className="text-8xl font-bold text-[#FBF6E3]">COFFEE TABLE</h5>
+        <h5 className="text-9xl font-bold text-[#FBF6E3]">COFFEE TABLE</h5>
       </div>
       <div className="flex mt-4">
         <div className="w-4/6 p-4">
-          <div className="flex h-[270px] gap-4">
+          <div className="flex h-[500px] gap-4">
             <Link to="/calendar" className="w-4/6">
               <div className="h-full p-10 w-full rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-base">
-                <h3 className="text-3xl text-brown font-bold ml-7 font-pathway">
+                <h3 className="text-6xl text-brown font-bold ml-7 font-pathway">
                   Calendar
                 </h3>
-                <h4 className="text-lg text-[#7B7E9D] font-bold mb-2 ml-7 italic font-pathway">
+                <h4 className="text-4xl text-[#7B7E9D] font-bold mb-2 ml-7 italic font-pathway">
                   Scheduling and Planning
                 </h4>
-                <p className="text-sm mt-5 mb-2 pt-4 pr-6 ml-7 text-brown font-montserrat">
+                <p className="text-xl mt-5 mb-2 pt-4 pr-6 ml-7 text-brown font-montserrat">
                   Create and manage schedules, and view the latest events on a
                   timeline. Your family can come together, sit down, and discuss
                   schedules collaboratively.
@@ -41,7 +41,7 @@ const Homepage = () => {
             </Link>
           </div>
           <Link to="/games">
-            <div className="h-[200px] w-full mt-6 p-10 rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-pink">
+            <div className="h-[620px] w-full mt-6 p-10 rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-pink">
               <h3 className="text-3xl font-bold text-brown ml-7 font-pathway">
                 Games
               </h3>
@@ -56,12 +56,12 @@ const Homepage = () => {
         </div>
         <div className="w-2/6 p-4">
           <Link to="/community">
-            <div className="h-[495px] w-full p-10 rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-yellow">
+            <div className="h-[1000px] w-full p-10 rounded-[30px] shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-yellow">
               <h3 className="text-3xl font-bold text-brown ml-7 font-pathway">
-                Dashboard
+                Community Board
               </h3>
               <h4 className="text-lg text-[#7B7E9D] font-bold mb-2 italic ml-7 font-pathway">
-                Community dashboard
+                Event dashboard
               </h4>
               <p className="text-sm mb-2 mt-10 pt-4 ml-7 mr-7 text-brown font-montserrat">
                 All important events and dates are posted in the form of
