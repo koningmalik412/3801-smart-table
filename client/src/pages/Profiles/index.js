72876b391852336768a2af4657afe35d424cdbd6
Malik Ismail <malikismail.0402@gmail.com>
2024-10-11 16:36:46 +1000

    Finalize Profile Feature

--- client/src/pages/Profiles/index.js
+++ client/src/pages/Profiles/index.js
@@ -1,27 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, forwardRef } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const imageOptions = [
+  require("../../assets/images/profile/profile_icon_1.png"),
+  require("../../assets/images/profile/profile_icon_2.png"),
+  require("../../assets/images/profile/profile_icon_3.png"),
+  require("../../assets/images/profile/profile_icon_4.png"),
+  require("../../assets/images/profile/profile_icon_5.png"),
+  require("../../assets/images/profile/profile_icon_6.png"),
+  require("../../assets/images/profile/profile_icon_7.png"),
+  require("../../assets/images/profile/profile_icon_8.png"),
+  require("../../assets/images/profile/profile_icon_9.png"),
+];
+
+const roleOptions = ["Dad", "Mom", "Son", "Daughter", "Friend"];
 
 const Modal = ({ onOpen, onClose, editingProfile }) => {
   const [name, setName] = useState("");
   const [fullName, setFullName] = useState("");
   const [role, setRole] = useState("");
-  const [dob, setDob] = useState("");
+  const [dob, setDob] = useState(null);
   const [image, setImage] = useState("");
 
   useEffect(() => {
     if (editingProfile) {
+      // Edit mode
       setName(editingProfile.name);
       setRole(editingProfile.role);
-      setDob(editingProfile.dob);
+      setDob(Date.parse(editingProfile.dob));
       setFullName(editingProfile.fullName);
       setImage(editingProfile.image);
+    } else {
+      // Add mode
+      setName("");
+      setRole("");
+      setDob(null);
+      setFullName("");
+      setImage("");
     }
   }, [editingProfile]);
+
+  const DateInputBar = forwardRef(({ value, onClick, className }, ref) => (
+    <div className={className} onClick={onClick} ref={ref}>
+      {value || <p className="text-placeholder-gray">Date of Birth</p>}
+    </div>
+  ));
 
   const handleSubmit = async () => {
     const profileData = { name, fullName, role, dob, image };
     if (editingProfile) {
-      console.log(JSON.stringify(profileData));
       await fetch(`http://localhost:3001/api/profiles/${editingProfile.id}`, {
         method: "PUT",
         headers: {
@@ -68,41 +96,59 @@ const Modal = ({ onOpen, onClose, editingProfile }) => {
           <input
             type="text"
             placeholder="Name"
-            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl"
+            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl placeholder-placeholder-gray"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
           <input
             type="text"
             placeholder="Full Name"
-            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl"
+            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl placeholder-placeholder-gray"
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
           />
-          <input
-            type="text"
-            placeholder="Role"
-            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl"
+
+          <select
+            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl placeholder-placeholder-gray"
             value={role}
             onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="">Select Role</option>
+            {roleOptions.map((option, index) => {
+              return <option value={index + 1}>{option}</option>;
+            })}
+          </select>
+
+          <DatePicker
+            selected={dob}
+            onChange={(date) => setDob(date)}
+            showYearDropdown
+            customInput={
+              <DateInputBar className="mt-4 w-[720px] p-4 rounded-xl bg-lightblue text-3xl" />
+            }
           />
-          <input
-            type="text"
-            placeholder="Date of Birth"
-            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl"
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Image"
-            className="mt-4 w-full p-4 rounded-xl bg-lightblue text-3xl"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-          />
+
+          <p className="text-2xl text-brown mt-6 text-center">
+            Select an Avatar:
+          </p>
+          <div className="grid grid-cols-3 gap-4 mt-4 justify-center">
+            {imageOptions.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                alt={`Avatar ${index + 1}`}
+                className={`${
+                  image === img
+                    ? "border-[5px] border-pink"
+                    : "border-[3px] border-brown"
+                } w-24 h-24 rounded-full  cursor-pointer mx-auto `}
+                onClick={() => setImage(img)}
+              />
+            ))}
+          </div>
         </div>
 
-        <div className="mt-6 flex justify-end">
+        <div className="mt-6 flex justify-center">
           <button
             onClick={handleSubmit}
             className="px-6 py-4 bg-blue text-base rounded-lg text-2xl"
@@ -154,7 +200,7 @@ const Profiles = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-between p-8">
+    <div className="min-h-screen flex flex-col items-center p-8">
       <div className="w-full p-6 bg-brown border rounded-[30px] shadow flex justify-between items-center">
         <h5 className="text-9xl font-bold text-[#FBF6E3]">FAMILY PROFILES</h5>
         <div
@@ -170,20 +216,24 @@ const Profiles = () => {
         </div>
       </div>
 
-      <div className="w-full min-h-[900px] mt-6 p-8 border-4 border-brown rounded-3xl bg-lightblue flex flex-wrap justify-center items-center">
+      <div className="w-full min-h-[900px] mt-16 p-8 border-4 border-brown rounded-3xl bg-lightblue flex flex-wrap justify-center items-center">
         {profiles.map((profile) => (
           <div
             key={profile.id}
             className="h-90 w-[calc(50%-20px)] p-10 m-2 rounded-3xl shadow-[0px_4px_15px_rgba(0,0,0,0.5)] bg-base flex flex-col items-center"
           >
             <img
-              className="w-24 h-24 mb-3 rounded-full shadow-lg"
+              className="w-32 h-32 mb-3 rounded-full border-4 border-brown shadow-lg"
               src={profile.image || ""}
               alt={profile.name}
             />
             <h5 className="mb-1 text-6xl text-blue">{profile.name}</h5>
-            <span className="text-2xl text-brown">{profile.role}</span>
-            <p className="text-2xl text-brown">Date of Birth: {profile.dob}</p>
+            <span className="text-2xl text-brown">
+              {roleOptions[profile.role - 1]}
+            </span>
+            <p className="text-2xl text-brown">
+              Date of Birth: {profile.dob?.slice(0, 10) || ""}
+            </p>
             <p className="text-2xl text-brown">Full Name: {profile.fullName}</p>
 
             <div className="flex justify-center space-x-5 mt-4">
@@ -209,7 +259,6 @@ const Profiles = () => {
         onClose={closeModal}
         editingProfile={editingProfile}
       />
-
     </div>
   );
 };
