0f0368ac296c8dfe3815abcbad249bf66a7f798c
Malik Ismail <malikismail.0402@gmail.com>
2024-10-11 12:24:14 +1000

    Games press fix

--- client/src/pages/Games/index.js
+++ client/src/pages/Games/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import chessboard from "../../assets/images/chess-board.jpeg";
 import snakes from "../../assets/images/snakes-and-ladders.png";
 import Chess from "../Games/chessboard.js";
@@ -10,7 +10,7 @@ const EnlargedChessboard = ({ onClose }) => {
     <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
       <div className="relative">
         <Chess />
-        <button 
+        <button
           onClick={onClose}
           className="absolute top-4 right-4 bg-white text-black px-4 py-2 rounded-full"
         >
@@ -25,12 +25,12 @@ const EnlargedSnakesAndLadders = ({ onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
       <div className="relative">
-        <img 
-          src={snakes} 
-          alt="Enlarged Snakes and Ladders Board" 
+        <img
+          src={snakes}
+          alt="Enlarged Snakes and Ladders Board"
           className="max-w-full max-h-[90vh] object-contain"
         />
-        <button 
+        <button
           onClick={onClose}
           className="absolute top-4 right-4 bg-white text-black px-4 py-2 rounded-full"
         >
@@ -66,34 +66,51 @@ const Games = () => {
 
       <div className="flex flex-col md:flex-row justify-center items-stretch w-full h-[1000px]">
         <div className="w-full md:w-1/2 p-4">
-          <div className="h-full bg-pink rounded-[30px] border-[3px] border-brown overflow-hidden">
-            <div className="h-[500px] cursor-pointer" onClick={handleChessboardClick}>
+          <div
+            className="h-full bg-pink rounded-[30px] border-[3px] border-brown overflow-hidden"
+            onClick={handleChessboardClick}
+          >
+            <div className="h-[500px] cursor-pointer">
               <Chess />
             </div>
             <div className="p-4 text-center">
-              <h2 className="text-9xl font-bold mb-2 text-darkbrown pt-20">Chess</h2>
-              <p className="text-brown text-5xl">Click the chess board to enlarge!</p>
+              <h2 className="text-9xl font-bold mb-2 text-darkbrown pt-20">
+                Chess
+              </h2>
+              <p className="text-brown text-5xl">
+                Click the chess board to enlarge!
+              </p>
             </div>
           </div>
         </div>
         <div className="w-full md:w-1/2 p-4">
-          <div className="h-full bg-yellow rounded-[30px] border-[3px] border-brown overflow-hidden">
-            <img 
-              src={snakes} 
-              alt="Snake and Ladder Board" 
+          <div
+            className="h-full bg-yellow rounded-[30px] border-[3px] border-brown overflow-hidden"
+            onClick={handleSnakesClick}
+          >
+            <img
+              src={snakes}
+              alt="Snake and Ladder Board"
               className="w-full h-[500px] object-cover cursor-pointer"
-              onClick={handleSnakesClick}
             />
             <div className="p-4 text-center">
-              <h2 className="text-9xl font-bold mb-2 text-darkbrown pt-20">Snakes and Ladders</h2>
-              <p className="text-brown text-5xl">Click the Snakes and Ladders board to enlarge!</p>
+              <h2 className="text-9xl font-bold mb-2 text-darkbrown pt-20">
+                Snakes and Ladders
+              </h2>
+              <p className="text-brown text-5xl">
+                Click the Snakes and Ladders board to enlarge!
+              </p>
             </div>
           </div>
         </div>
       </div>
 
-      {showEnlargedChessboard && <EnlargedChessboard onClose={handleCloseEnlarged} />}
-      {showEnlargedSnakes && <EnlargedSnakesAndLadders onClose={handleCloseEnlarged} />}
+      {showEnlargedChessboard && (
+        <EnlargedChessboard onClose={handleCloseEnlarged} />
+      )}
+      {showEnlargedSnakes && (
+        <EnlargedSnakesAndLadders onClose={handleCloseEnlarged} />
+      )}
     </div>
   );
 };
