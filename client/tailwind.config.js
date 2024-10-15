26b3ee8bc73cbd108f1dd683268e6c2f4cdf4628
Malik Ismail <malikismail.0402@gmail.com>
2024-10-15 16:45:11 +1000

    Fix side calendar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-calendar/dist/Calendar.css",
+  ],
   theme: {
     extend: {},
     colors: {
