91c36f6fdfc52ef96486b0b2622ca5920664d2c9
Malik Ismail <malikismail.0402@gmail.com>
2024-10-17 12:36:12 +1000

    Change standby button to return to previous screen

--- client/src/pages/Standby/index.js
+++ client/src/pages/Standby/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { useNavigate } from "react-router-dom"; // Import Link from react-router-dom
 
 const Standby = () => {
-    return (
-        <Link 
-            to="/" 
-            className="bg-black h-screen w-screen fixed top-0 left-0 flex items-center justify-center z-50" // Adjust position and z-index
-        >
-        </Link>
-    );
+  const router = useNavigate();
+  return (
+    <button
+      onClick={() => router(-1)}
+      className="bg-black h-screen w-screen fixed top-0 left-0 flex items-center justify-center z-50" // Adjust position and z-index
+    ></button>
+  );
 };
 
 export default Standby;
